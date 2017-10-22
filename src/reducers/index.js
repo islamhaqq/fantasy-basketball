e78888478d2b55c85a7f264efe9c0d3fc54d42d7
Syed Aman <sna274@nyu.edu>
2017-10-22 08:51:24 -0500

    create the root reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux'
+
+/**
+ * Gets application level state by combining all reducers into one central
+ * location.
+ * @type {Object}
+ */
+const rootReducer = combineReducers({
+  // add in imported reducers
+})
+
+export default rootReducer
