f1840985e9b34548f54527dc0723cd92f3d3ea00
Syed Aman <sna274@nyu.edu>
2017-10-22 13:58:37 -0500

    create reducer for user's roster

---
+++ src/reducers/roster.js
@@ -0,0 +1,9 @@
+/**
+ * Fetches state containing an array of all the players hired by the user into
+ * the user's franchise.
+ * @method
+ * @return {Array} - All the players hired into the franchise.
+ */
+export default function() {
+  return []
+}
