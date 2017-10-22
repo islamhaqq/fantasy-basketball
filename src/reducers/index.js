4f87cad1175dfe050d8a49fbcaa59dd4a11184b6
Syed Aman <sna274@nyu.edu>
2017-10-22 08:56:30 -0500

    add in the free agents reducer to the root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux'
 
+import FreeAgents from './free-agents'
+
 /**
  * Gets application level state by combining all reducers into one central
  * location.
@@ -7,6 +9,7 @@ import { combineReducers } from 'redux'
  */
 const rootReducer = combineReducers({
   // add in imported reducers
+  freeAgents: FreeAgents
 })
 
 export default rootReducer
