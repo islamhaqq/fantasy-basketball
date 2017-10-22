30d0a77866e5d7614f6c764a6b3b0884b5b41599
Syed Aman <sna274@nyu.edu>
2017-10-22 10:49:43 -0500

    create an action creator to hire players

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+/**
+ * An action creator that creates an action which adds players to the roster.
+ * @method hirePlayer
+ * @param  {Object}   player - An available NBA player from free agents.
+ * @return {Object} The action.
+ */
+export function hirePlayer(player) {
+  // create the action
+}
