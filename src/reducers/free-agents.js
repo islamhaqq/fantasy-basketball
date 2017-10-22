b1afe22324d92bbd2be45ecbae5521744db2bd5a
Syed Aman <sna274@nyu.edu>
2017-10-22 10:22:57 -0500

    add some data to the free agents reducer

--- src/reducers/free-agents.js
+++ src/reducers/free-agents.js
@@ -7,5 +7,17 @@
 export default function () {
   return [
     // all the available players to be hired i.e. "free agents"
+    {
+      email: 'lebronjames@cavaliers.com',
+      firstName: 'Lebron',
+      lastName: 'James',
+      photo: 'http://i.cdn.turner.com/nba/nba/.element/img/2.0/sect/statscube/players/large/lebron_james.png'
+    },
+    {
+      email: 'chrispaul@clippers.com',
+      firstName: 'Chris',
+      lastName: 'Paul',
+      photo: 'http://static1.1.sqspcdn.com/static/f/360296/19968996/1345620638217/LAC_Paul_Chris.jpg?token=4Gc3%2BxP%2Faagl7KParPsltTirnBw%3D'
+    },
   ]
 }
