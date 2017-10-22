a9c13aa39ac261c378b249f5ec65319343b00d98
Syed Aman <sna274@nyu.edu>
2017-10-22 08:53:39 -0500

    create a free agents reducer

---
+++ src/reducers/free-agents.js
@@ -0,0 +1,11 @@
+/**
+ * Fetches state containing an array of all the players that are available
+ * for hiring into the user's franchise/club.
+ * @method
+ * @return {Array} - All the players available for hire.
+ */
+export default function () {
+  return [
+    // all the available players to be hired i.e. "free agents"
+  ]
+}
