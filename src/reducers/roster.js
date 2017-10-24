704ee0754ef916d16f8af9c2393663c7e66a9cb6
Syed Aman <sna274@nyu.edu>
2017-10-24 04:59:20 -0500

    add switch case statements to the roster reducer

--- src/reducers/roster.js
+++ src/reducers/roster.js
@@ -2,8 +2,16 @@
  * Fetches state containing an array of all the players hired by the user into
  * the user's franchise.
  * @method
+ * @param {Object} state - The players hired in the user's franchise.
+ * @param {Function} action - The tasks to perform to modify the state.
  * @return {Array} - All the players hired into the franchise.
  */
-export default function() {
-  return []
+export default function(state, action) {
+  switch (action.type) {
+    case 'HIRE_PLAYER':
+      return action.payload
+  }
+
+  // don't do anything to state and return the original by default
+  return state
 }
